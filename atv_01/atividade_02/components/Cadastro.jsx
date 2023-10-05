879f1947c2c2b96d0c9bf25d9d98cc930c07a3a1
JerlyJr <42943873+JerlyJr@users.noreply.github.com>
2023-10-04 23:30:42 -0300

    Preparativos

---
+++ atv_01/atividade_02/components/Cadastro.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const Cadastro = () => {
+    return(
+        <View>
+            <Text>
+            </Text>
+        </View>
+    )
+}
---
+++ atv_01/atividade_02/components/Main.jsx
@@ -0,0 +1,3 @@
+import { Text, View, } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
